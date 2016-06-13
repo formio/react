e4afce05d884c9e116769bc5af15ba11c560be00
Abhishek Thorat <abhishek.thorat@tudip.nl>
2016-06-13 22:45:51 +0530

    Added the support for the webpack 	1. Created two configuration file as webpack-formio.config.js and webpack-app.config.js 	2. Webpack-formio.config.js contain the configuration code for the formio script. 	3. Webpack-app.config.js contain the config code for the example app. 	4. Created three different tasks in gulp accordingly, 		i) webpack_formio :- This task will execute same as gulp script in browserify. 		ii) webpack_app :- This task will execute same as gulp app browserify. 		iii) build_webpack :- This task will execute same as gulp serve browserify. 	5. Added dev dependancies for 		i)babel-core 		ii)babel-loader 		iii)babel-preset-es2015 		iv)babel-preset-react 		v)gulp-sourcemaps 		vi)webpack-stream 	6. SelectMixin.js file's changes was committed in other branch (react-currency-component) but still committing 		same changes in this branch because those changes are not merged yet.

---
+++ webpack-app.config.js
@@ -0,0 +1,49 @@
+var uglify = require('gulp-uglify');
+
+module.exports = function(gulp, plugins) {
+    return function () {
+        function handleErrors() {
+            var args = Array.prototype.slice.call(arguments);
+            plugins.notify.onError({
+                title: 'Compile Error',
+                message: '<%= error.message %>'
+            }).apply(this, args);
+            this.emit('end'); // Keep gulp from hanging on this task
+        }
+
+        return gulp.src('./src/app.js')
+            .pipe(plugins.webpack({
+                output: {
+                    filename: "app.js"
+                },
+                module: {
+                    loaders: [
+                        {
+                            test: /require\.js$/,
+                            loader: "exports?requirejs!script"
+                        },
+                        {
+                            test: /\.jsx?$/,
+                            loader: 'babel',
+                            exclude: /node_modules/,
+                            query: {
+                                presets: ['react', 'es2015']
+                            }
+                        }
+                    ],
+                    resolve: {
+                        extensions: ['', '.js', '.jsx'],
+                        alias: {
+                            requirejs$:  "./dist/example/"
+                        },
+                    },
+                    noParse: [
+                        /node_modules\/formiojs\//,
+                    ],
+
+                }
+            }))
+            .pipe(plugins.streamify(plugins.uglify()))
+            .pipe(gulp.dest('./dist/example/'));
+    }
+}
