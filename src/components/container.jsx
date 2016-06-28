c077eccf3cb88fc5bba677db6ebd9f27b6e2d780
Randall Knutson <randall@form.io>
2016-06-27 22:51:40 -0500

    Fix formatting and remove array from data structure.

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -3,41 +3,42 @@ var valueMixin = require('./mixins/valueMixin.jsx');
 var FormioComponent = require('../FormioComponent.jsx');
 
 module.exports = React.createClass({
-    displayName: 'Container',
-    mixins: [valueMixin],
-    getInitialValue: function() {
-        return [{}];
-    },
-    elementChange: function(index, component) {
-        var value = this.state.value;
-        value[index][component.props.component.key] = component.state.value;
-        this.setState({
-            value: value
-        });
-        this.props.onChange(this);
-    },
-    getElements: function() {
-        var classLabel = 'control-label' + ( this.props.component.validate && this.props.component.validate.required ? ' field-required' : '');
-        var inputLabel = (this.props.component.label && !this.props.component.hideLabel ? <label htmlFor={this.props.component.key} className={classLabel}>{this.props.component.label}</label> : '');
-        return (
-            <div className='formio-container'>
-                {this.props.component.components.map(function(component, index) {
-                    var value = (this.props.data && this.props.data.hasOwnProperty(component.key) ? this.props.data[component.key] : component.defaultValue || '');
-                    var key = (component.key) ? component.key : component.type;
-                    return (
-                    <div key={index}>
-                        <FormioComponent
-                            {...this.props}
-                            key={key}
-                            name={component.key}
-                            component={component}
-                            onChange={this.elementChange.bind(null, index)}
-                            value={value}
-                        />
-                    </div>
-                    );
-                }.bind(this))}
+  displayName: 'Container',
+  mixins: [valueMixin],
+  getInitialValue: function () {
+    return {};
+  },
+  elementChange: function (component) {
+    var value = this.state.value;
+    value[component.props.component.key] = component.state.value;
+    this.setState({
+      value: value
+    });
+    this.props.onChange(this);
+  },
+  getElements: function () {
+    var classLabel = 'control-label' + ( this.props.component.validate && this.props.component.validate.required ? ' field-required' : '');
+    var inputLabel = (this.props.component.label && !this.props.component.hideLabel ?
+      <label htmlFor={this.props.component.key} className={classLabel}>{this.props.component.label}</label> : '');
+    return (
+      <div className='formio-container'>
+        {this.props.component.components.map(function (component) {
+          var value = (this.state.value && this.state.value.hasOwnProperty(component.key) ? this.state.value[component.key] : component.defaultValue || '');
+          var key = (component.key) ? component.key : component.type;
+          return (
+            <div key={key}>
+              <FormioComponent
+                {...this.props}
+                key={key}
+                name={component.key}
+                component={component}
+                onChange={this.elementChange}
+                value={value}
+              />
             </div>
-        );
-    }
+          );
+        }.bind(this))}
+      </div>
+    );
+  }
 });
