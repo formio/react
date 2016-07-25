64be264adbb538cd67d1a198a108bd454c146b37
Nick Hall <nick.hall@bluelogicteam.com>
2016-07-25 18:11:52 -0500

    WIP Commit. Added blue button to current page. Working on debugging next button

--- src/FormioWizard.jsx
+++ src/FormioWizard.jsx
@@ -19,14 +19,18 @@ module.exports = React.createClass({
     };
   },
   nextPage: function(event) {
-    console.log('click');
-    event.preventDefault();
-    this.setState(function(previousState) {
-      if (previousState.currentPage < (numPages - 1)) {
-        previousState.currentPage++;
-      }
-      return previousState;
-    });
+    try {
+      console.log('click');
+      event.preventDefault();
+      this.setState(function(previousState) {
+        if (previousState.currentPage < (numPages - 1)) {
+          previousState.currentPage++;
+        }
+        return previousState;
+      });
+    } catch(e){
+      console.log(e);
+    }
   },
   previousPage: function() {
     this.setState((previousState) => {
@@ -40,47 +44,56 @@ module.exports = React.createClass({
     var pages = this.state.pages;
     var pageArray = [];
 
+    console.log(this.props.nextPage);
+
+
     for(var i = 0; i < pages.length;i++){
-      var blueButton =
-        <a className="bs-wizard-dot bg-primary">
-          <div className="bs-wizard-dot-inner bg-success"></div>
-        </a>;
+      var curPageClassName = 'col-sm-2 bs-wizard-step';
+      var innerDot = 'bs-wizard-dot-inner';
+
+      if(i === this.state.currentPage){
+        curPageClassName += ' active';
+        innerDot += ' bg-success';
+      } else {
+        curPageClassName += ' disabled';
+      }
 
       pageArray.push(
-        <div className="bs-wizard-step">
+        <div className={curPageClassName}>
           <div className="text-center bs-wizard-stepnum">
             {pages[i].title}
           </div>
           <div className="progress">
             <div className="progress-bar progress-bar-primary"></div>
           </div>
+          <a className="bs-wizard-dot bg-primary">
+            <div className={innerDot}></div>
+          </a>
         </div>
       )
     }
 
-    // Add class active to current page section
-    if(pages[this.state.currentPage]){
-      console.log('test');
-    }
-
     return (
       <div className="formio-wizard">
-          <div className="panel-body">
+          <div className="panel panel-default">
             <div className="panel-heading">
               <h3 className="panel-title">Preview</h3>
             </div>
-            <div className="row bs-wizard hasTitles">
-              {pageArray}
-            </div>
-            <FormioComponents
-              {...this.props}
-              components = {this.state.pages[this.state.currentPage].components}
-            >
-            </FormioComponents>
-            <button
-              onClick={this.nextPage}
-              type="button"
-            >Next</button>
+            <div className="panel-body">
+              <div className="formio-wizard-wrapper">
+                <div className="row bs-wizard hasTitles">
+                  {pageArray}
+                </div>
+                <FormioComponents
+                  {...this.props}
+                  components = {this.state.pages[this.state.currentPage].components}
+                >
+                </FormioComponents>
+                <button onClick={console.log('this is firing')}
+                  type="button"
+                >NEXT BUTTON</button>
+                </div>
+              </div>
           </div>
       </div>
     );
