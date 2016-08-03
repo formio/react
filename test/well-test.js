18fee19e52274e05a57a9af57e568c25bb94ceaf
Abhishek Thorat <abhishek.thorat@tudip.nl>
2016-08-03 18:16:03 +0530

    Testcase for the well component. 1. For that added the well-test.js file to the module and added the implementation for following scenarios .
          Renders a basic well component
          Check the nested components of well
    
    2. Also added the componentSpec.js file to the module to mock the components.
    3. Please update us if we are missing out any scenario to be test.
    4. Same changes related to the componentSpec.js file is also committed on branch RE-52-table-component-testcase and on RE-32-container-component-testcase.

---
+++ test/well-test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import Well from '../src/components/well.jsx';
+import sinon from 'sinon';
+import form from './forms/empty.json';
+
+var components = require('./forms/componentSpec.js');
+
+describe('Well', function () {
+  describe(' Well component', function () {
+    var component= {
+      "lockKey": true,
+      "key": "well",
+      "conditional": {
+        "eq": "",
+        "when": null,
+        "show": ""
+      },
+      "type": "well",
+      "components": [
+        components.textfeild,
+        components.password,
+        components.phoneNumber
+      ],
+      "input": false
+    };
+    var attachToForm = sinon.spy();
+
+    it('Renders a basic well component', function (done) {
+      const element = shallow(<Well
+        component={component}
+        attachToForm={attachToForm}
+        {...this.props} />)
+      expect(element.find('.well').length).to.equal(1);
+      done();
+    });
+
+    it('Check the nested components of well', function (done) {
+      const element = shallow(<Well
+        component={component}
+        attachToForm={attachToForm}
+        {...this.props} />).find('.well');
+
+      //To test type of nested components of well
+      for (var i= 0; i<component.components.length; i++) {
+        expect(element.nodes[0].props.children.props.component.components[i].type).to.equal(component.components[i].type);
+      }
+      done();
+    });
+
+  });
+
+});
