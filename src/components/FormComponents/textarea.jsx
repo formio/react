52acfa137a70301f9a1726fcdd8542c62094fd63
Randall Knutson <randall@form.io>
2017-03-15 18:59:01 -0500

    Add ability to use wysiwyg with textarea.

--- src/components/FormComponents/textarea.jsx
+++ src/components/FormComponents/textarea.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactQuill from 'react-quill';
 import valueMixin from './mixins/valueMixin';
 import multiMixin from './mixins/multiMixin';
 import componentMixin from './mixins/componentMixin';
@@ -6,22 +7,52 @@ import componentMixin from './mixins/componentMixin';
 module.exports = React.createClass({
   displayName: 'Textarea',
   mixins: [valueMixin, multiMixin, componentMixin],
+  customState: function(state) {
+    const { component, readOnly } = this.props;
+    this.isWysiwig = false;
+    if (!readOnly && component.wysiwyg) {
+      this.isWysiwig = true;
+    }
+    return state;
+  },
+  onChangeWysiwyg: function(index, value) {
+    this.setValue(value, index);
+  },
   getSingleElement: function(value, index) {
+    const { component, name, readOnly } = this.props;
     index = index || 0;
-    return (
-      <textarea
-        className="form-control"
-        key={index}
-        id={this.props.component.key}
-        data-index={index}
-        name={this.props.name}
-        value={value}
-        disabled={this.props.readOnly}
-        placeholder={this.props.component.placeholder}
-        rows={this.props.component.rows}
-        onChange={this.onChange}
-      >
-      </textarea>
-    );
+    if (this.isWysiwig) {
+      return (
+        <ReactQuill
+          key={index}
+          id={component.key}
+          data-index={index}
+          name={name}
+          value={value}
+          theme="snow"
+          disabled={readOnly}
+          placeholder={component.placeholder}
+          rows={component.rows}
+          onChange={this.onChangeWysiwyg.bind(null, index)}
+        />
+      );
+    }
+    else {
+      return (
+        <textarea
+          className="form-control"
+          key={index}
+          id={component.key}
+          data-index={index}
+          name={name}
+          value={value}
+          disabled={readOnly}
+          placeholder={component.placeholder}
+          rows={component.rows}
+          onChange={this.onChange}
+        >
+        </textarea>
+      );
+    }
   }
 });
