ca06ce153b83e5c9da0947b6e725c0359ce07da1
Randall Knutson <randall@form.io>
2017-06-15 17:11:36 -0500

    Update auth.

--- src/modules/auth/containers/Auth.js
+++ src/modules/auth/containers/Auth.js
@@ -3,23 +3,27 @@ import {Link} from 'react-router';
 
 export default function Auth(props) {
   return (
-    <div className="panel panel-default">
-      <div className="panel-heading" style={{paddingBottom: 0, borderBottom: 'none'}}>
-        <ul className="nav nav-tabs" style={{borderBottom: 'none'}}>
-          <li role="presentation"><Link to="/auth/login">Login</Link></li>
-          <li role="presentation"><Link to="/auth/register">Register</Link></li>
-        </ul>
-      </div>
-      <div className="panel-body">
-        <div className="row">
-          <div className="col-lg-12">
-            {props.children}
-          </div>
+  <div className="row">
+    <div className="col-md-4 col-md-offset-4">
+      <div className="panel panel-default">
+        <div className="panel-heading" style={{paddingBottom: 0, borderBottom: 'none'}}>
+          <ul className="nav nav-tabs" style={{borderBottom: 'none'}}>
+            <li role="presentation"><Link to="/auth/login">Login</Link></li>
+            <li role="presentation"><Link to="/auth/register">Register</Link></li>
+          </ul>
         </div>
-        <div className="row">
-          <div className="col-lg-12"></div>
+        <div className="panel-body">
+          <div className="row">
+            <div className="col-lg-12">
+              {props.children}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12"></div>
+          </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
