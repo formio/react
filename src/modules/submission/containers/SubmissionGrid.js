018f579f041ecfcad1c0e787939d0eb2bd1dea24
Randall Knutson <randall@form.io>
2018-02-20 14:28:10 -0600

    Update location of build files to lib.

--- src/modules/submission/containers/SubmissionGrid.js
+++ src/modules/submission/containers/SubmissionGrid.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import FormioUtils from 'formiojs/utils';
-import Components from 'formiojs/build/components';
+import Components from 'formiojs/lib/components';
 import _get from 'lodash/get';
 import Grid from '../../../components/Grid';
 
