7b46a4a6c737f0220d92b9be3854a3e05253cb0a
Yury Rybak <yuryrybak97@gmail.com>
2019-03-07 13:42:24 +0300

    Small improvements.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import Webform from 'formiojs/Webform';
+import WebformBuilder from 'formiojs/WebformBuilder';
+import Wizard from 'formiojs/Wizard';
+import WizardBuilder from 'formiojs/WizardBuilder';
+import nestedEditForm from 'formiojs/components/nested/NestedComponent.form';
+
+export {
+  Webform,
+  WebformBuilder,
+  Wizard,
+  WizardBuilder,
+  nestedEditForm,
+};
+
 export * from './components';
 export * from './modules';
-export {Components, Formio} from 'formiojs';
+export {Components, Formio, Utils} from 'formiojs';
