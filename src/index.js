a95b4b17818f34d5c3965df8b71ee11920ca4c56
Randall Knutson <randall@knutson.io>
2019-03-20 10:14:23 -0500

    Bump formio and remove problem import.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import Webform from 'formiojs/Webform';
 import WebformBuilder from 'formiojs/WebformBuilder';
 import Wizard from 'formiojs/Wizard';
 import WizardBuilder from 'formiojs/WizardBuilder';
-import editFormUtils from 'formiojs/components/base/editForm/utils';
+// import editFormUtils from 'formiojs/components//editForm/utils';
 
 export {
   Webform,
   WebformBuilder,
   Wizard,
   WizardBuilder,
-  editFormUtils,
+  // editFormUtils,
 };
 
 export * from './components';
