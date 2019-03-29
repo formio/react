b6405f84ff8b322ee201a2985391ae84570b367b
VladTsygankov <Uladzislau.Tsyhankou@itechart-group.com>
2019-03-29 12:52:18 +0300

    Added adaprtive layout for more than 12 columns

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -22,9 +22,9 @@ export default class extends Component {
   };
 
   static defaultProps = {
-    onSort: () => {},
-    onAction: () => {},
-    onPage: () => {},
+    onSort: () => { },
+    onAction: () => { },
+    onPage: () => { },
     firstItem: 1,
     lastItem: 1,
     total: 1,
@@ -42,80 +42,143 @@ export default class extends Component {
   render = () => {
     const {items, columns, columnWidths, sortOrder, onSort, emptyText, Cell, perms} = this.props;
     const {firstItem, lastItem, total, activePage, onPage, pages, pageNeighbours} = this.props;
-    return (
-      <div>
-        { items.length ?
-          <ul className='list-group list-group-striped'>
-            <li className='list-group-item list-group-header hidden-xs hidden-md'>
-              <div className='row'>
-                { columns.map((column, index) => {
+
+    if (items.length && columns.length > 12) {
+      return (<div>
+        <div style={{maxWidth: '100%', overflowX: 'scroll'}}>
+          <table className="table">
+            <thead>
+              <tr>
+                {columns.map((column, index) => {
                   if (!column.sort) {
                     return (
-                      <div key={index} className={'col col-md-' + columnWidths[index]}>
-                        <strong>{ column.title }</strong>
-                      </div>
+                      <td key={index} style={index === columns.length - 1 ? {position:  'sticky', right: 0, background: 'white'} : {}} >
+                        <strong>{column.title}</strong>
+                      </td>
                     );
                   }
                   let sortClass = '';
                   if (sortOrder === column.key) {
-                    sortClass = 'glyphicon glyphicon-triangle-top fa fa-caret-up';
+                    sortClass = 'glyphicon glyphicon-triangle-top fa fa-caret-u';
                   }
                   else if (sortOrder === ('-' + column.key)) {
-                    sortClass = 'glyphicon glyphicon-triangle-bottom fa fa-caret-down';
+                    sortClass = 'glyphicon glyphicon-triangle-top fa fa-caret-up';
                   }
+
                   return (
-                    <div
+                    <td
                       key={index}
-                      className={'col col-md-' + columnWidths[index]}
-                      style={{cursor: 'pointer'}}
+                      style={index === columns.length - 1 ? {position:  'sticky', right: 0, cursor: 'pointer', background: 'white'} : {}}
                     >
                       <span
                         style={{cursor: 'pointer'}}
                         onClick={() => onSort(column.key)}
                       >
-                        <strong>{ column.title } <span className={sortClass}/></strong>
+                        <strong>{column.title} <span className={sortClass} /></strong>
                       </span>
-                    </div>
+                    </td>
                   );
                 })}
-              </div>
-            </li>
-            { items.map((item, index) => {
-              return (
-                <li className='list-group-item' key={item._id}>
-                  <div className='row' onClick={() => {
-                    if (perms.data) {
-                      return this.props.onAction(item, 'row');
-                    }
-                  }}>
-                    { columns.map((column, index) => {
-                      return (
-                        <div key={index} className={'col col-md-' + columnWidths[index]}>
-                          <Cell row={item} column={column} {...this.props} />
-                        </div>
-                      );
-                    })}
+              </tr>
+            </thead>
+            <tbody>
+              {items.map(item => (
+                <tr key={item._id} onClick={() => this.props.onAction(item, 'row')}>
+                  {columns.map((column, index) => {
+                    return (
+                      <td key={index} style={{position: index === columns.length - 1 ? 'sticky' : 'inherit', right: 0, background: 'white'}}>
+                        <Cell row={item} column={column} {...this.props} />
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <span className="pull-left">
+          <Pagination
+            pages={pages}
+            activePage={activePage}
+            pageNeighbours={pageNeighbours}
+            prev="Previous"
+            next="Next"
+            onSelect={onPage}
+          />
+        </span>
+        <span className="pull-right item-counter"><span className="page-num">{firstItem} - {lastItem}</span> / {total} total</span>
+      </div>);
+    }
+
+    if (items.length) {
+      return (<div>
+        <ul className='list-group list-group-striped'>
+          <li className='list-group-item list-group-header hidden-xs hidden-md'>
+            <div className='row'>
+              {columns.map((column, index) => {
+                if (!column.sort) {
+                  return (
+                    <div key={index} className={'col col-md-' + columnWidths[index]}>
+                      <strong>{column.title}</strong>
+                    </div>
+                  );
+                }
+                let sortClass = '';
+                if (sortOrder === column.key) {
+                  sortClass = 'glyphicon glyphicon-triangle-top fa fa-caret-up';
+                }
+                else if (sortOrder === ('-' + column.key)) {
+                  sortClass = 'glyphicon glyphicon-triangle-bottom fa fa-caret-down';
+                }
+                return (
+                  <div
+                    key={index}
+                    className={'col col-md-' + columnWidths[index]}
+                    style={{cursor: 'pointer'}}
+                  >
+                    <span
+                      style={{cursor: 'pointer'}}
+                      onClick={() => onSort(column.key)}
+                    >
+                      <strong>{column.title} <span className={sortClass} /></strong>
+                    </span>
                   </div>
-                </li>
-              );
-            })}
-            <li className="list-group-item">
-              <span className="pull-left">
-                <Pagination
-                  pages={pages}
-                  activePage={activePage}
-                  pageNeighbours={pageNeighbours}
-                  prev="Previous"
-                  next="Next"
-                  onSelect={onPage}
-                />
-              </span>
-              <span className="pull-right item-counter"><span className="page-num">{ firstItem } - { lastItem }</span> / { total } total</span>
-            </li>
-          </ul> :
-          <div>{emptyText}</div>
-        }
-      </div>
-    );
+                );
+              })}
+            </div>
+          </li>
+          {items.map((item, index) => {
+            return (
+              <li className='list-group-item' key={item._id}>
+                <div className='row' onClick={() => this.props.onAction(item, 'row')}>
+                  {columns.map((column, index) => {
+                    return (
+                      <div key={index} className={'col col-md-' + columnWidths[index]}>
+                        <Cell row={item} column={column} {...this.props} />
+                      </div>
+                    );
+                  })}
+                </div>
+              </li>
+            );
+          })}
+          <li className="list-group-item">
+            <span className="pull-left">
+              <Pagination
+                pages={pages}
+                activePage={activePage}
+                pageNeighbours={pageNeighbours}
+                prev="Previous"
+                next="Next"
+                onSelect={onPage}
+              />
+            </span>
+            <span className="pull-right item-counter"><span className="page-num">{firstItem} - {lastItem}</span> / {total} total</span>
+          </li>
+        </ul>
+      </div>);
+    }
+
+    return (<div>{emptyText}</div>);
   }
 }
