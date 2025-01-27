f33709ea53921d612b7bf5003021dc08d1c82fce
Brendan J Bond <brendanbond@gmail.com>
2025-01-27 10:24:32 -0600

    fix case issue in test environment setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: './FixJSDOMEnvironment.ts',
+	testEnvironment: './fixJSDOMEnvironment.ts',
 	testPathIgnorePatterns: [
 		'/node_modules/',
 		'/lib/',
