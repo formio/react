99709964eeb2989650b7ed0f7626f8c5cd5256cb
lane-formio <lane@form.io>
2025-04-20 23:07:51 -0500

    Updated build: @formio/react@6.1.0-rc.1

--- Changelog.md
+++ Changelog.md
@@ -5,7 +5,12 @@ All notable changes to this project will be documented in this file
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [Unreleased: 6.1.0-rc.1]
+## 6.1.0-rc.1
+
+### Changed
+
+-   Updated @formio/js@5.2.0-rc.1
+-   Updated @formio/core@2.5.0-rc.1
 
 ## 6.0.0
 
--- src/components/FormEdit.tsx
+++ src/components/FormEdit.tsx
@@ -38,16 +38,18 @@ const DEFAULT_INITAL_FORM = {
 	path: '',
 	display: 'form' as const,
 	type: 'form' as const,
-	components: [{
-		type: 'button',
-		label: 'Submit',
-		key: 'submit',
-		size: 'md',
-		block: false,
-		action: 'submit',
-		disableOnInvalid: true,
-		theme: 'primary'
-	}],
+	components: [
+		{
+			type: 'button',
+			label: 'Submit',
+			key: 'submit',
+			size: 'md',
+			block: false,
+			action: 'submit',
+			disableOnInvalid: true,
+			theme: 'primary',
+		},
+	],
 };
 
 export const DEFAULT_SETTINGS_FORM = {
@@ -124,7 +126,7 @@ export const DEFAULT_SETTINGS_FORM = {
 													{
 														label: 'Wizard',
 														value: 'wizard',
-													}
+													},
 												],
 											},
 											validate: {
@@ -243,7 +245,7 @@ export const FormEdit = ({
 			onSaveForm?.(form);
 		} catch (error) {
 			console.error('Error saving form', error);
-			setError(error as ErrorObject) ;
+			setError(error as ErrorObject);
 		}
 	};
 
